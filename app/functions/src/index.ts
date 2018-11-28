42da39eb2610bcace83f9f11c0aadbaa37e623a8
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-11-28 23:33:44 +0900

    chore: server side rendering using nuxt

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,8 +1,26 @@
+import * as express from "express";
 import * as functions from 'firebase-functions';
+import { Nuxt } from "nuxt";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+const app = express();
+
+const nuxt = new Nuxt({
+  dev: false,
+  buildDir: "./lib/.nuxt",
+  build: {
+    publicPath: "/assets/"
+  }
 });
+
+function handleRequest(req, res) {
+  res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
+  return new Promise((resolve, reject) => {
+    nuxt.render(req, res, (promise) => {
+      promise.then(resolve).catch(reject);
+    });
+  });
+}
+
+app.use(handleRequest);
+
+export const render = functions.https.onRequest(app);
