37e5fcc3e232e4a5a95c7c798ec425e6c0e413c5
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-01 04:54:09 +0900

    fix: change name

--- app/app/components/BlogFooter.vue
+++ app/app/components/BlogFooter.vue
@@ -3,7 +3,7 @@
     .py-5
       p.text-white.text-center
         small
-          nuxt-link.text-white.no-underline(class="hover:underline" to="/") blog.mochizuki.moe
+          nuxt-link.text-white.no-underline(class="hover:underline" to="/") みかづきメモ
           br
           | Powered by 
           a.text-white.no-underline(class="hover:underline" href="https://github.com/mika-f/blog.mochizuki.moe") Yui
--- app/app/components/BlogTitle.vue
+++ app/app/components/BlogTitle.vue
@@ -2,7 +2,7 @@
   .pt-5.pb-10
     .py-5
       nuxt-link.no-underline.text-black(class="hover:underline" to="/")
-        h1 blog.mochizuki.moe
+        h1 みかづきメモ
     
     small.text-grey-darker
       | プログラミング学習帳 ♪(✿╹ヮ╹)ﾉ 書いてあるコードは自己責任でご自由にどうぞ
