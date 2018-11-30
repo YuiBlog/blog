f4f5893aed245e0d01cd1387969c3e5950b608fe
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-01 04:56:44 +0900

    fix: change name

--- app/app/nuxt.config.js
+++ app/app/nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: "みかづきメモ",
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
