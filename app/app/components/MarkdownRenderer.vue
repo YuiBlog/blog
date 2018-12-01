98462446d78f97195b26883326da714dca8d8405
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-01 19:09:52 +0900

    feat: markdown renderer

---
+++ app/app/components/MarkdownRenderer.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  div(v-html="asHtmlDocument")
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+import Marked from "marked";
+import Prism from "prismjs";
+
+// languages
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-diff";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-nginx";
+import "prismjs/components/prism-perl";
+import "prismjs/components/prism-pug";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-ruby";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-vim";
+import "prismjs/components/prism-yaml";
+
+// theme
+import "prismjs/themes/prism-okaidia.css";
+
+@Component
+export default class extends Vue {
+  private renderer: Marked.Renderer;
+
+  public constructor() {
+    super();
+    this.renderer = new Marked.Renderer();
+    this.renderer.codespan = code => `<code class="font-mono rounded bg-black text-white p-1">${code}</code>`;
+    this.renderer.listitem = text => `<li class="leading-normal">${text}</li>\n`;
+    this.renderer.paragraph = text => `<p class="my-4 leading-normal">${text}</p>\n`;
+
+    Marked.setOptions({
+      gfm: true,
+      highlight: (code, lang) => {
+        return Prism.languages[lang] ? Prism.highlight(code, Prism.languages[lang]) : code;
+      }
+    });
+  }
+
+  @Prop({ required: true })
+  public markdown!: string;
+
+  public get asHtmlDocument(): string {
+    return Marked.parse(this.markdown, { renderer: this.renderer });
+  }
+}
+</script>
--- app/app/pages/entry/_year/_month/_day/_id.vue
+++ app/app/pages/entry/_year/_month/_day/_id.vue
@@ -4,54 +4,20 @@
       small.text-xs.m-2 date
     h2.py-2 title
 
-    div(v-html="asHtmlDocument")
+    markdown-renderer(markdown="markdown")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
 
-import Marked from "marked";
-import Prism from "prismjs";
+import MarkdownRenderer from "@/components/MarkdownRenderer.vue";
 
-// 使ってそうなものだけ入れておく
-import "prismjs/components/prism-bash";
-import "prismjs/components/prism-csharp";
-// import "prismjs/components/prism-cpp";
-import "prismjs/components/prism-diff";
-import "prismjs/components/prism-json";
-import "prismjs/components/prism-nginx";
-import "prismjs/components/prism-perl";
-import "prismjs/components/prism-pug";
-import "prismjs/components/prism-python";
-import "prismjs/components/prism-tsx";
-import "prismjs/components/prism-jsx";
-import "prismjs/components/prism-ruby";
-import "prismjs/components/prism-typescript";
-import "prismjs/components/prism-vim";
-import "prismjs/components/prism-yaml";
-
-import "prismjs/themes/prism-okaidia.css";
-
-@Component
-export default class extends Vue {
-  public get asHtmlDocument(): string {
-    // customize for tailwindcss.
-    const renderer = new Marked.Renderer();
-    renderer.codespan = code => `<code class="font-mono rounded bg-black text-white p-1">${code}</code>`;
-    renderer.listitem = text => `<li class="leading-normal">${text}</li>\n`;
-    renderer.paragraph = text => `<p class="my-4 leading-normal">${text}</p>\n`;
-
-    Marked.setOptions({
-      gfm: true,
-      highlight: (code, lang) => {
-        if (Prism.languages[lang]) {
-          return Prism.highlight(code, Prism.languages[lang]);
-        }
-        return code;
-      }
-    });
-
-    return Marked.parse("", { renderer });
+@Component({
+  components: {
+    MarkdownRenderer
   }
+})
+export default class extends Vue {
+  public markdown!: string;
 }
 </script>
