dc23570c52db8b5f39f0940aec28ff2519ac8ffb
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-03 13:33:20 +0900

    fix: access via property `entry`.

--- app/functions/src/https/api.ts
+++ app/functions/src/https/api.ts
@@ -1,10 +1,9 @@
 import * as cors from "cors";
 import * as express from "express";
-import * as firebase from "firebase-admin";
 import * as functions from 'firebase-functions';
 
 import "../bootstrap/initialize";
-import * as query from "../shared/entry";
+import * as query from "../shared/query";
 
 const app = express();
 const corsOptions: cors.CorsOptions = {
@@ -15,14 +14,14 @@ const corsOptions: cors.CorsOptions = {
 app.options("*", cors(corsOptions));
 
 app.get("/api/entries", cors(corsOptions), async (req, res) => {
-  const entries = await query.list(0);
+  const entries = await query.entry.list(0);
 
   res.set("Cache-Control", "public, max-age=60, s-maxage=300");
   res.status(200).send(entries);
 });
 
 app.get("/api/entries/:yyyy/:mm/:slug", cors(corsOptions), async (req, res) => {
-  const entry = await query.show(req.params.yyyy, req.params.mm, req.params.slug);
+  const entry = await query.entry.show(req.params.yyyy, req.params.mm, req.params.slug);
 
   res.set("Cache-Control", "public, max-age=60, s-maxage=300");
   res.status(200).send(entry);
