c63caf1ddb0587eb4fa2af9a349fd7508536972f
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-14 08:31:19 +0900

    bug: list returns 5 entries

--- app/shared/entry.ts
+++ app/shared/entry.ts
@@ -3,7 +3,6 @@ import * as firebase from "firebase-admin";
 import { Entries, Entry } from "./models/entry";
 
 export async function show(year: number, month: number, slug: string): Promise<Entry> {
-  let entry!: Entry;
   const collection = await firebase.firestore()
     .collection("entries")
     .where("created_at", ">=", new Date(year, month - 1))
@@ -11,9 +10,7 @@ export async function show(year: number, month: number, slug: string): Promise<E
     .where("slug", "==", slug)
     .limit(1)
     .get();
-  collection.forEach(w => entry = w.data() as Entry);
-
-  return entry;
+  return collection.docs.shift().data() as Entry;
 }
 
 export async function list(offset: number): Promise<Entries> {
@@ -22,12 +19,12 @@ export async function list(offset: number): Promise<Entries> {
     .collection("entries")
     .orderBy("created_at", "desc")
     .limit(6)
-    .offset(offset)
+    .offset(offset * 5)
     .get();
   collection.forEach(entry => entries.push(entry.data() as Entry));
 
   return {
-    entries,
+    entries: entries.slice(0, 5),
     offset,
     hasPrev: offset > 0,
     hasNext: entries.length > 5
