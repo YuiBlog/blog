34110a2c0ad9b906a00ce65f82f622929bad6464
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-16 02:30:53 +0900

    fix: fix typo

--- app/app/nuxt.config.js
+++ app/app/nuxt.config.js
@@ -22,7 +22,7 @@ module.exports = {
     ],
     link: [
       {rel: "icon", href: "/favicon.ico"},
-      {rel: "apple-touch-icon", sizes: "180x180", href: "apple-touch-icon.png"},
+      {rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
       {rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
       {rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
       {rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5"},
