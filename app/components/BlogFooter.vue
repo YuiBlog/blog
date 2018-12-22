f2337e4792e3ea525abc788aa450c9e3312f182e
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-23 03:35:47 +0900

    fix: fix link

--- app/components/BlogFooter.vue
+++ app/components/BlogFooter.vue
@@ -6,7 +6,7 @@
           nuxt-link.text-white(to="/") みかづきメモ
           br
           | Powered by 
-          a.text-white(href="https://github.com/mika-f/blog.mochizuki.moe" target="_blank") Yui
+          a.text-white(href="https://github.com/YuiBlog" target="_blank") Yui
           | .
           | The source code is licensed 
           a.text-white(href="http://opensource.org/licenses/mit-license.php" target="_blank") MIT
