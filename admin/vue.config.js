b46e816be0a9b463f64f53a344cdad718d3c51f5
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-28 13:55:31 +0900

    chore: configure monaco-editor features

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,12 +1,54 @@
 const DotEnv = require("dotenv-webpack");
+const MonacoEditor = require("monaco-editor-webpack-plugin");
 
 module.exports = {
   baseUrl: process.env.NODE_ENV === "production" ? "/admin/" : "/",
   chainWebpack: (config) => {
     config.module
       .rule('pug')
-      .use('pug-plain-loader')
+      .use('pug-plain-loader');
 
-    config.plugin("dotenv-webpack").use(DotEnv)
+    config.plugin("dotenv-webpack").use(DotEnv);
+    config.plugin("monaco-editor").use(MonacoEditor, [{
+      languages: ["markdown"],
+      features: [
+        "accessibilityHelp",
+        "bracketMatching",
+        "caretOperations",
+        "clipboard",
+        "codeAction",
+        "colorDetector",
+        "comment",
+        "contextmenu",
+        "coreCommands",
+        "cursorUndo",
+        "dnd",
+        "find",
+        "folding",
+        "format",
+        "gotoError",
+        "gotoLine",
+        "hover",
+        "inPlaceReplace",
+        "inspectTokens",
+        "iPadShowKeyboard",
+        "linesOperations",
+        "links",
+        "multicursor",
+        "parameterHints",
+        "quickCommand",
+        "quickOutline",
+        "referenceSearch",
+        "rename",
+        "smartSelect",
+        "snippets",
+        "suggest",
+        "toggleHighContrast",
+        "toggleTabFocusMode",
+        "transpose",
+        "wordHighlighter",
+        "wordOperations"
+      ]
+    }]);
   }
 }
