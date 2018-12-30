d31de1aa342a9a733eb9af5708f908ffaf76772a
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-12-30 22:22:51 +0900

    feat: title input on editor

--- admin/src/views/Edit.vue
+++ admin/src/views/Edit.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
-  tab-horizontal.h-full.max-h-screen.p-4
-    tab-pane.flex-1.flex.py-2(name="編集")
-      .flex-1
-        markdown-editor(v-model="body")
+  tab-horizontal.h-full.max-h-screen.p-2
+    tab-pane.flex-1.flex.pt-2(name="編集")
+      .flex-1.flex.flex-col
+        .pb-2
+          input.w-full.border.py-3.px-2(v-model="title" placeholder="タイトル")
+        markdown-editor.flex-1(v-model="body")
       .w-64.px-2
-        .border-grey
+        tab-vertical.h-full
+          tab-pane(name="画像" cls="far fa-images")
+            p Hello, World
+          tab-pane(name="カテゴリー" cls="fas fa-tags")
+          tab-pane(name="オプション" cls="fas fa-cog")
     tab-pane.flex-1.py-2.overflow-y-scroll(name="プレビュー")
       markdown-previewer(:markdown="body")
 </template>
@@ -14,6 +20,7 @@ import { Component, Vue } from "vue-property-decorator";
 
 import TabHorizontal from "@/components/tabs/Horizontal.vue";
 import TabPane from "@/components/tabs/Pane.vue";
+import TabVertical from "@/components/tabs/Vertical.vue";
 
 import MarkdownEditor from "@/components/MarkdownEditor.vue";
 import MarkdownPreviewer from "@/components/MarkdownPreviewer.vue";
@@ -23,7 +30,8 @@ import MarkdownPreviewer from "@/components/MarkdownPreviewer.vue";
     MarkdownEditor,
     MarkdownPreviewer,
     TabHorizontal,
-    TabPane
+    TabPane,
+    TabVertical
   }
 })
 export default class Edit extends Vue {
@@ -46,5 +54,20 @@ export default class Edit extends Vue {
 <style lang="postcss" scoped>
 .h-full.max-h-screen {
   max-height: calc(100vh - 64px);
+}
+
+input {
+  @apply appearance-none;
+  @apply block;
+  @apply w-full;
+  @apply text-grey-darkest;
+  @apply border border-grey rounded;
+  @apply py-3 px-4;
+  @apply leading-tight;
+}
+
+input:focus {
+  @apply outline-none;
+  @apply border-grey-darker;
 }
 </style>
